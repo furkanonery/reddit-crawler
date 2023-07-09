1c3bde8dad22cd4a1936527a7a638dc2a2cb69b1
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-07-09 17:38:12 +0300

    The scraped fields were expanded.

--- core/crawler_soup/api/views.py
+++ core/crawler_soup/api/views.py
@@ -1,14 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from crawler_soup.api.serializers import SubredditsSerializer
-from crawler_soup.models import Subreddits
+from crawler_soup.models import SubredditPost
 from .task import getposts
 
 
 class SubredditsView(APIView):
     def get(self, request, format=None):
 
-        queryset = Subreddits.objects.all()
+        queryset = SubredditPost.objects.all()
 
         serializer = SubredditsSerializer(queryset, many=True)
 
--- core/crawler_soup/models.py
+++ core/crawler_soup/models.py
@@ -13,3 +13,20 @@ class Subreddits(models.Model):
         if existing_subreddits.exists():
             return
         super().save(*args, **kwargs)
+
+class SubredditPost(models.Model):
+    title = models.CharField(max_length=255)
+    vote = models.IntegerField()
+    author = models.CharField(max_length=255)
+    text = models.TextField(null=True)
+    img = models.CharField(max_length=255, null=True)
+    datetime = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        existing_subreddits = SubredditPost.objects.filter(title=self.title)
+        if existing_subreddits.exists():
+            return
+        super().save(*args, **kwargs)
