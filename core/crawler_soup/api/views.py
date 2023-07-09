ef64d710d49df779c47aa9038cac0ab0bad508d9
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-07-09 23:32:01 +0300

    A permission has been defined for the 'postsSoup' endpoint.

--- core/crawler_soup/api/views.py
+++ core/crawler_soup/api/views.py
@@ -3,9 +3,13 @@ from rest_framework.response import Response
 from crawler_soup.api.serializers import SubredditsSerializer
 from crawler_soup.models import SubredditPost
 from .task import getposts
+from rest_framework.permissions import IsAuthenticated
 
 
 class SubredditsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, format=None):
 
         queryset = SubredditPost.objects.all()
